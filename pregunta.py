eaf8b5d1337e6f377ef5ea8b56139cb5ec0900b3
mmunozal <55594437+mmunozal@users.noreply.github.com>
2022-10-17 18:49:03 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -13,7 +13,7 @@ from datetime import datetime
 def clean_data():
     df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col = 0)
     df.dropna(axis = 0, inplace = True)
-    df.drop_duplicates(inplace = True)
+    
     df[["sexo", "tipo_de_emprendimiento","idea_negocio","barrio","línea_credito"]]=df[["sexo", "tipo_de_emprendimiento","idea_negocio","barrio","línea_credito"]].apply(lambda x: x.astype(str).str.lower())
     for i in ['tipo_de_emprendimiento', 'idea_negocio', 'línea_credito', 'barrio']:
         df[i] = df[i].apply(lambda x: x.replace('_', ' '))
@@ -24,5 +24,5 @@ def clean_data():
 
     df = df.astype({ "monto_del_credito": int, "comuna_ciudadano": float})
     df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
-
+    df.drop_duplicates(inplace = True)
     return df
